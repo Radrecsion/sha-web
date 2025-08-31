35ad3ea8cf10bb0d0cf1464c3597ea4ebe794e51
Radrecsion <radi35fir@gmail.com>
2025-08-31 20:18:59 +0700

    fix fe railway 5

--- psha-web/server.js
+++ psha-web/server.js
@@ -8,10 +8,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Serve static files dari folder build Vite
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Catch-all: semua route lain dikirim ke index.html
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
