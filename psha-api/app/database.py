6eb8ea9debae786134d076a9db795d8ab54a7bd2
Radrecsion <radi35fir@gmail.com>
2025-09-01 16:04:51 +0700

    fix 502 fe 7 (error db)

--- psha-api/app/database.py
+++ psha-api/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models.base import Base  # import dari base.py, jangan dari models lain
 
-DATABASE_URL = "postgresql+psycopg2://psha:psha@db:5432/pshadb"
+DATABASE_URL = "postgresql+psycopg2://postgres:PIjMWkSHXZzFGcwprGpoUUcmdKRXKQwA@shortline.proxy.rlwy.net:48540/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
