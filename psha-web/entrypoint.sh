a724353db9f3c7a0acbc12199510479dd860be40
Radrecsion <radi35fir@gmail.com>
2025-09-01 13:42:34 +0700

    fix 502 fe 2

--- psha-web/entrypoint.sh
+++ psha-web/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
-
+echo "VITE_API_URL=$VITE_API_URL"
 # Ganti placeholder config.js dengan environment variable Railway
 envsubst < /usr/share/nginx/html/config.js > /usr/share/nginx/html/config.runtime.js
 mv /usr/share/nginx/html/config.runtime.js /usr/share/nginx/html/config.js
 
 # Start nginx (foreground)
+
 exec nginx -g 'daemon off;'
